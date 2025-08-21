18bc4b82d9dc022761abca6edd2eeb059aa09d6e
jeannegris <jean.negris@gmail.com>
2025-08-21 14:15:47 -0300

    Config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // Configurações globais da aplicação
 
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "http://140.238.187.229:8000";
 
 // Futuramente, basta alterar o valor acima para o IP externo ou usar variáveis de ambiente
